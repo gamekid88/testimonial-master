a5897bb80ae692842eeb4218bd7ae72a42d268d5
Frank Corso <fpcorso@gmail.com>
2015-02-28 12:31:10 -0500

    Update JS

--- js/admin.js
+++ js/admin.js
@@ -27,3 +27,40 @@ function tm_validateForm()
     return false;
   }
 }
+
+function tm_delete_testimonial(id)
+{
+  jQuery('#delete_testimonial').val(id);
+  document.delete_testimonial_form.submit();
+}
+
+function tm_edit_testimonial(id)
+{
+  testimonial = jQuery("#content_"+id).html();
+  who = jQuery("#name_"+id).html();
+  where = jQuery("#link_"+id).html();
+
+  jQuery('#edit_testimonial_id').val(id);
+  jQuery('#edit_testimonial').val(testimonial);
+  jQuery('#edit_who').val(who);
+  jQuery('#edit_where').val(where);
+
+  jQuery('#edit_testimonial_form').show();
+  jQuery('#new_testimonial_form').hide();
+}
+
+function tm_setTab(tab) {
+  jQuery("a.nav-tab-active").toggleClass("nav-tab-active");
+  if (tab == 1)
+  {
+    jQuery("#what_new").show();
+    jQuery("#changelog").hide();
+    jQuery("#tab_1").toggleClass("nav-tab-active");
+  }
+  if (tab == 2)
+  {
+    jQuery("#what_new").hide();
+    jQuery("#changelog").show();
+    jQuery("#tab_2").toggleClass("nav-tab-active");
+  }
+}
